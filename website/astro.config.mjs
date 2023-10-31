68fb4159e385867119f3545767cccf1d4397d15c
Adam Lane <enalmada@gmail.com>
2023-10-31 11:18:59 -0700

    website (#33)

---
+++ website/astro.config.mjs
@@ -0,0 +1,50 @@
+import prefetch from '@astrojs/prefetch';
+import react from '@astrojs/react';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'next-secure',
+      defaultLocale: 'root', // optional
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en', // lang is required for root locales
+        },
+      },
+      social: {
+        github: 'https://github.com/Enalmada/next-secure',
+      },
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [
+            {
+              label: 'Getting Started',
+              link: '/guides/getting-started/',
+            },
+          ],
+        },
+        {
+          label: 'Technologies',
+          items: [
+            {
+              label: 'Summary',
+              link: '/technologies/summary/',
+            },
+          ],
+        },
+      ],
+    }),
+    react(),
+    // applyBaseStyles causes lists to not work anymore
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    prefetch(),
+  ],
+});
---
+++ website/src/content/docs/technologies/summary.md
@@ -0,0 +1,7 @@
+---
+title: Technologies
+description: Technologies used.
+---
+
+## Technologies used
+* [next-safe](https://www.npmjs.com/package/next-safe) next-safe helps secure your Next.js apps by providing sensible defaults for the most common security headers
